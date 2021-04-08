b8020bac20b6467d320e6d9dd56209b758b024e7
rebeccatricker <rebeccaatricker@gmail.com>
2021-04-08 14:27:37 -0500

    Adding in code for both UFO Levels

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+// Use d3 to update each cell's text with
+// table values (date/time, city, state, country, shape, comment)
+data.forEach(function(ufoSighting) {
+  console.log();ufoSighting
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+}); 
+
+// Select the button 
+var button = d3.select("#filter-btn");
+
+// Create event handlers and complete the event handler function
+// Prevent page from refreshing
+// Select the input element and get the raw HTML node
+// Get the value property of the input element
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+
+var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+filteredData.forEach(function(date) {
+    var row = tbody.append("tr");
+    Object.entries(date).forEach(function([key, value]){
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+});
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,63 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+// Use d3 to update each cell's text with
+// table values (date/time, city, state, country, shape, comment)
+data.forEach(function(ufoSighting) {
+  console.log();ufoSighting
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+}); 
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers and complete the event handler function
+// Prevent page from refreshing
+// Select the input element and get the raw HTML node
+// Get the value property of the input element
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+var inputCity = d3.select("#city").property("value");
+var inputState = d3.select("#state").property("value");
+var inputCountry = d3.select("#country").property("value");
+var inputShape = d3.select("#shape").property("value");
+
+var filteredData = tableData;
+if (inputValue) {
+    filteredData = filteredData.filter(row => row.datetime === inputValue);
+}
+if (inputCity) {
+    filteredData = filteredData.filter(row => row.city === inputCity);
+}
+if (inputState) {
+    filteredData = filteredData.filter(row => row.state === inputState);
+}
+if (inputCountry) {
+    filteredData = filteredData.filter(row => row.country === inputCountry);
+}
+if (inputShape) {
+    filteredData = filteredData.filter(row => row.shape === inputShape);
+}
+
+filteredData.forEach(function(sections) {
+    var row = tbody.append("tr");
+    Object.entries(sections).forEach(function([key, value]){
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+   });
+});
